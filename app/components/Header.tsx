0e967fbea48519d2e77bb5929ccd2164c75142a9
gsdutra <gabrielsprecherdutra@gmail.com>
2023-07-23 18:42:39 -0300

    feat: better header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,17 +8,21 @@ const pathway = Pathway_Gothic_One({
 
 export default function Header() {
   return (<>
-    <div className="h-[8rem] w-full bg-black fixed top-0 flex flex-col justify-center items-center text-white z-20">
-      <div className={pathway.className}>
-        <a className="text-7xl">ISLÂNDIA</a>
+    <div className="h-[8rem] w-full bg-black fixed top-0 flex justify-between items-center text-white z-20 px-10">
+      <img src="/bandeira.png" width={100}/>
+      <div className="flex flex-col justify-center items-center">
+        <div className={pathway.className}>
+          <a className="text-7xl">ISLÂNDIA</a>
+        </div>
+
+        <div className="underline flex">
+          <Link href="/" className="mx-2">Home</Link>
+          <Link href="/about" className="mx-2">Sobre o servidor</Link>
+          <Link href="/community" className="mx-2">Comuninade</Link>
+          <Link href="/contact" className="mx-2">Entrar no servidor</Link>
+        </div>
       </div>
-      <div className="underline flex">
-        <Link href="/" className="mx-2">Home</Link>
-        <Link href="/gallery" className="mx-2">Galeria</Link>
-        <Link href="/history" className="mx-2">História</Link>
-        <Link href="/discussion" className="mx-2">Discussão</Link>
-        <Link href="/contact" className="mx-2">Entrar no servidor</Link>
-      </div>
+      <img src="/brasao.png" width={100}/>
     </div>
   </>)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
-import { Lato } from "next/font/google"
+import { Roboto_Condensed } from "next/font/google"
 
-const lato = Lato({
+const lato = Roboto_Condensed({
   weight: '400',
   subsets: ['latin']
 })
@@ -15,8 +15,15 @@ export default function Home() {
       alt="Islândia"
     />
     <div className="bg-gradient-to-t from-black to-transparent fixed w-full h-full top-10"></div>
-    <div className="fixed w-full h-full flex justify-center items-center text-white text-3xl">
+    <div className="fixed w-full h-full flex flex-col justify-center items-center text-white text-3xl">
+      <img
+        src="/brasao.png"
+        width={400}
+        height={400}
+        alt="Islândia"
+        className="mb-4"
+      />
       <a className={lato.className}>Conheça o melhor servidor de Minecraft da história</a>
-      </div>
+    </div>
   </>)
 }
