2b40185c9c70d349ac5414f3569497afa9776d14
gsdutra <gabrielsprecherdutra@gmail.com>
2023-07-23 17:41:11 -0300

    init commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Pathway_Gothic_One } from "next/font/google"
+import Link from "next/link"
+
+const pathway = Pathway_Gothic_One({
+  weight: '400',
+  subsets: ['latin']
+})
+
+export default function Header() {
+  return (<>
+    <div className="h-[8rem] w-full bg-black fixed top-0 flex flex-col justify-center items-center text-white">
+      <div className={pathway.className}>
+        <a className="text-7xl">ISLÂNDIA</a>
+      </div>
+      <div className="underline flex">
+        <Link href="/" className="mx-2">Home</Link>
+        <Link href="/gallery" className="mx-2">Galeria</Link>
+        <Link href="/history" className="mx-2">História</Link>
+        <Link href="/contact" className="mx-2">Entrar no servidor</Link>
+      </div>
+    </div>
+  </>)
+}
