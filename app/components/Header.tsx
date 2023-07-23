418c388ef88a0035b02d9a8e2ea0bf26bbc8d231
gsdutra <gabrielsprecherdutra@gmail.com>
2023-07-23 17:55:34 -0300

    feat: add main page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ const pathway = Pathway_Gothic_One({
 
 export default function Header() {
   return (<>
-    <div className="h-[8rem] w-full bg-black fixed top-0 flex flex-col justify-center items-center text-white">
+    <div className="h-[8rem] w-full bg-black fixed top-0 flex flex-col justify-center items-center text-white z-20">
       <div className={pathway.className}>
         <a className="text-7xl">ISLÂNDIA</a>
       </div>
@@ -16,6 +16,7 @@ export default function Header() {
         <Link href="/" className="mx-2">Home</Link>
         <Link href="/gallery" className="mx-2">Galeria</Link>
         <Link href="/history" className="mx-2">História</Link>
+        <Link href="/discussion" className="mx-2">Discussão</Link>
         <Link href="/contact" className="mx-2">Entrar no servidor</Link>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image"
+import { Lato } from "next/font/google"
+
+const lato = Lato({
+  weight: '400',
+  subsets: ['latin']
+})
+
 export default function Home() {
   return (<>
-    
+    <Image
+      src="/print1.jpg"
+      layout='fill'
+      objectFit='cover'
+      alt="Islândia"
+    />
+    <div className="bg-gradient-to-t from-black to-transparent fixed w-full h-full top-10"></div>
+    <div className="fixed w-full h-full flex justify-center items-center text-white text-3xl">
+      <a className={lato.className}>Conheça o melhor servidor de Minecraft da história</a>
+      </div>
   </>)
 }
